49124120391a429ee387e9e189beeeed52ccdff0
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-16 17:53:53 -0300

    Route update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 
+
 app = Flask(__name__)
+account_list = {}
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -9,15 +11,43 @@ def index():
   return render_template("index.html")
 
 
-@app.route("/create-account.html", methods=["POST","GET"])
-def account():
-    return render_template("create-account.html")
+@app.route("/add-account.html")
+def add_account():
+    return render_template("add-account.html")
 
-#@app.route("/create-account", methods=["POST"])
-#def create_account():
-#  name=request.form.get("name")
-#  account_balance=request.form.get("balance")
-#  return render_template("create-account.html", name=name, #account_balance=account_balance)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/create-account.html")
+def create_account():
+    #Validade name
+    account_owner = request.form.get("fname" + " " + "lname")
+    if not account_owner:
+      return render_template("error.html", message="Missing name")
+
+    #Validate account_balance
+    account_balance = request.form.get("balance")
+    if not account_balance:
+       return render_template("error.html", message="Missing balance")
+    
+
+
+@app.route("/remove-account.html")
+def remove_account():
+  return render_template("remove-account.html")
+
+
+@app.route("/see-account.html")
+def see_account():
+  return render_template("see-account.html")
+
+
+@app.route("/operation.html")
+def operation():
+  return render_template("operation.html")
+
+
+@app.route("/reports.html")
+def reports():
+  return render_template("reports.html")
+
+
+app.run(debug=True)
